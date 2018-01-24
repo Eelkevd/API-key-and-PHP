1fb5d486e1e756653ef613f834af618cd06e42c6
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-01-24 16:23:27 +0100

    Versie 1 (basic)
    Kan via kleine html form een echo krijgen met GET

---
+++ oefenen_front.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+</head>
+
+<body>
+	<form action="oefenen.php" method="get">
+	  ID: <input type="number" name="id" /><br /><br />
+	  MYKEY: <input type="text" name="mykey" /><br /><br />
+	  VALUE: <input type="text" name="value" /><br /><br />
+	  <input type="submit" name="submit" value="Submit" />
+	</form>
+</body>
+
+</html>
